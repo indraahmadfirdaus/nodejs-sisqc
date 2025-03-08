fef775ddc526b89175ff36da8f376a94040b8162
Indra <indraahmadfirdaus@gmail.com>
2025-03-08 16:39:21 +0700

    fix: cors

--- src/app.js
+++ src/app.js
@@ -20,7 +20,9 @@ connectDB();
 
 app.use(morgan('dev'))
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+}));
 app.use(express.json({ limit: '50mb'  }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
